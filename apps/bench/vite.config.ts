a16496cb2221c50de5bffbe1998a3d28f5f73f22
Andrey Baikov <bboyair@mail.ru>
2025-09-03 08:01:40 +0300

    pages deploy

--- apps/bench/vite.config.ts
+++ apps/bench/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/cnstra-oimdb-bench/',
   plugins: [react(), tsconfigPaths()],
 });
